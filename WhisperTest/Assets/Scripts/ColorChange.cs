a00a68fdff24c5505c5dcee1159a889dcb42d63f
Kevin Shi <kevincsh@usc.edu>
2017-03-11 15:59:24 -0800

    the box, it changes color

--- WhisperTest/Assets/Scripts/ColorChange.cs
+++ WhisperTest/Assets/Scripts/ColorChange.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class ColorChange : MonoBehaviour {
 
-	public Color colorStart;
-	public Color colorEnd;
 	public Renderer rend;
-	public float amp;
 	public float duration = 1.0F;
 
 	Color pink = new Color32(251,57,154,255);
@@ -17,21 +14,72 @@ public class ColorChange : MonoBehaviour {
 	Color blue = new Color32(94,212,247,255);
 	Color indigo = new Color32(76,153,248,255);
 	Color violet = new Color32(152,97,250,255);
-	Color black = new Color32(0,0,0, 255);
-	Color white = new Color32(255,255,255,255);
+    public Color[] colorMarkers;
+    
+    private List<float> ampHistory = new List<float>();
+    private int HISTORY_LENGTH = 24;
+    private float SCALE = 1.0f;
 
-	public Color lerpedColor;
+    /*private float TIME_DELAY = 0.1f;
+    private float progress = 0.0f;
+    private bool shouldCalculate = true;
+    private Color colorStart = Color.white;
+    private Color colorEnd = Color.white;*/
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		rend = GetComponent<Renderer>();
-		amp = GetComponent<SourceAmplitude> ().myVolume;
+        if (colorMarkers.Length == 0)
+        {
+            colorMarkers = new Color[7];
+            colorMarkers[0] = pink;
+            colorMarkers[1] = orange;
+            colorMarkers[2] = yellow;
+            colorMarkers[3] = green;
+            colorMarkers[4] = blue;
+            colorMarkers[5] = indigo;
+            colorMarkers[6] = violet;
+        }
 	}
 
 	// Update is called once per frame
-	void Update () {
-		lerpedColor = Color.Lerp(Color.white, Color.black, (amp * 3));
-		rend.material.color = lerpedColor;
-		print (lerpedColor);
-	}
+	void Update ()
+    {
+        /*if (shouldCalculate)
+        {
+            colorStart = colorEnd;
+            colorEnd = calcNewColor();
+            shouldCalculate = false;
+        }
+        else if (progress > TIME_DELAY)
+        {
+            progress -= TIME_DELAY;
+            shouldCalculate = true;
+        }
+        progress += Time.deltaTime;
+        rend.material.color = Color.Lerp(colorStart, colorEnd, Mathf.Clamp(progress / TIME_DELAY, 0, 1));*/
+        rend.material.color = calcNewColor();
+    }
+
+    Color calcNewColor()
+    {
+        float amp = GetComponent<SourceAmplitude>().myVolume * SCALE;
+        ampHistory.Add(amp);
+        if (ampHistory.Count > HISTORY_LENGTH)
+            ampHistory.RemoveAt(0);
+        float averageAmp = 0;
+        foreach (float a in ampHistory)
+        {
+            averageAmp += a;
+        }
+        averageAmp = averageAmp / ampHistory.Count;
+        int numMarkers = colorMarkers.Length;
+        int index = (int) Mathf.Floor(averageAmp * numMarkers);
+        if (index == numMarkers) index--;
+        float t = Mathf.Clamp01((averageAmp - (index / numMarkers)) * numMarkers);
+        if (index == numMarkers - 1)
+            return Color.Lerp(colorMarkers[index], colorMarkers[0], t);
+        else
+            return Color.Lerp(colorMarkers[index], colorMarkers[index + 1], t);
+    }
 }
