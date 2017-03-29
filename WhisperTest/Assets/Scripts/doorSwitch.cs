a98d421aa20253cdcbca63e888e490ad2d539c1a
Lex Rhodes <awrhodes@usc.edu>
2017-03-29 14:31:17 -0700

    updates to kirkwood's room scene

--- WhisperTest/Assets/Scripts/doorSwitch.cs
+++ WhisperTest/Assets/Scripts/doorSwitch.cs
@@ -8,12 +8,16 @@ public class doorSwitch : MonoBehaviour {
 	public bool switchTriggerOccupied;
 	public bool whichPos = true;
 
+	AudioSource doorSwitchAudio;
+
 	public float speed = 5F;
 	public float posOne = 289F;
 	public float posTwo = 109F;
 
 	// Use this for initialization
 	void Start () {
+
+		doorSwitchAudio = gameObject.GetComponent<AudioSource> ();
 
 	}
 	
@@ -22,15 +26,19 @@ public class doorSwitch : MonoBehaviour {
 
 		switchTriggerOccupied = switchTrigger.GetComponent<doorTrigger> ().triggerOccupied;
 
-		if (switchTriggerOccupied && whichPos == true) {
+		if (switchTriggerOccupied) {
+			doorSwitchAudio.Play ();
+		}
 
+		if (switchTriggerOccupied && whichPos == true) {
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, posOne, 0), Time.deltaTime * speed);
 		}
 			
 		if (switchTriggerOccupied && whichPos == false) {
-
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, posTwo, 0), Time.deltaTime * speed);
 		}
+
+
 
 		if (switchTrigger.GetComponent<doorTrigger>().triggerExit == true) {
 			whichPos = !whichPos;
