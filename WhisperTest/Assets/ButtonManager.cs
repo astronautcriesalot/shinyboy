377a1ef802cacc7ecd057a45d940aea5bd62d89c
unknown <sarvshrs@usc.edu>
2017-03-29 21:35:09 -0700

    Added door to exit level with password ~

---
+++ WhisperTest/Assets/ButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    bool hitaButton = false;
+    public int ButtonValue;
+    public bool rayHit = false;
+    public delegate void ButtonClick(int value);
+    public static event ButtonClick OnButtonClick;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnMouseEnter()
+    {
+        gameObject.layer = 1;
+        
+        Debug.Log("CAN INTERACT WITH BUTTON: " + ButtonValue);
+    }
+    void OnMouseExit()
+    {
+        gameObject.layer = 10;
+    }
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OnButtonClick(ButtonValue);
+        }
+    }
+}
